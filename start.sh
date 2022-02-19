49eca14c1e68ee1e1c2884829ee8e5e4cf5c7ff0
jcazako <jeancharles.cazako@gmail.com>
2022-02-19 17:28:08 +0100

    default project

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
-if [ $1 = 'js' ]
+if [ "$1" == "js" ]
 then
   echo 'Got a js project to start'
 fi
 
-if [ $1 = 'sh' ]
+if [ "$1" == "sh" ]
 then
   echo 'Got a shell project to start'
+else
+  echo 'Default project'
 fi
